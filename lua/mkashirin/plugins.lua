b59c46ad355ec7b1a6984aef4ebea2dc4661fe4e
mkashirin <m.k4shirin@yandex.ru>
2024-03-04 20:43:45 +0300

    Add refactoring.nvim to plugins

--- lua/mkashirin/plugins.lua
+++ lua/mkashirin/plugins.lua
@@ -102,4 +102,16 @@ return {
 
   -- Live execution
   { "rhysd/reply.vim" },
+
+    -- Refactoring features
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function ()
+        require("refactoring").setup()
+    end,
+  },
 }
